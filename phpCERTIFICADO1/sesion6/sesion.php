ca3fd9449f0699b94c66ef0c07962d1aaaa2b18b
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2025-01-13 21:50:04 -0300

    agregado error al iniciar sesion en pagina modificar.php (no tenia escrito el codigo de validar.php para validar si esta o no logeado y terminando la sesion si no)

--- phpCERTIFICADO1/sesion6/sesion.php
+++ phpCERTIFICADO1/sesion6/sesion.php
@@ -1,1 +1,9 @@
-    
+<?php
+session_start();
+if (!isset($_SESSION['activo']) || !$_SESSION['activo']) {
+    die("Acceso denegado. Debes iniciar sesión.");
+}
+
+echo "Bienvenido, usuario: " . $_SESSION['usuario'];
+?>
+
